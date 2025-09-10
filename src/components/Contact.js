984053d7880526ee671b9d990ff1a7d8054c6fbc
Eashaan Sharma <120644754+eashaan-sharma@users.noreply.github.com>
2025-09-10 22:36:59 +0530

    Custom mailing logic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -25,7 +25,7 @@ export const Contact = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText('Sending...');
-        let response = await fetch("http://localhost:3000/contact", {
+        let response = await fetch("/api/contact", {
             method: "POST",
             headers: {
                 "Content-Type": "Application/json;charset=utf-8",
